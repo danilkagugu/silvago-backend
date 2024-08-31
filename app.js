5803dc82743acf638f848e804db6a39f96fab84b
Danulo <danilyanishevski2001@gmail.com>
2024-08-31 15:42:40 +0300

    cors

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 const app = express();
 
+const corsOptions = {
+  origin: "https://silvago.vercel.app", // Додайте тут ваш домен
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 app.use(morgan("tiny"));
-app.use(cors());
-// app.use(express.json());
-app.use(express.json({ limit: "10mb" })); // Збільшити ліміт до 10 MB
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/api", router);
